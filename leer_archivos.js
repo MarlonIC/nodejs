5210bc63d20b7303dfc8b36a8e87f9ff27104792
root <root@debian.debian-linux>
2016-12-29 16:04:51 -0500

    Leyendo archivos

--- leer_archivos.js
+++ leer_archivos.js
@@ -1,9 +1,20 @@
 var http = require("http"),
 	fs = require("fs");
 
-var html = fs.readFileSync("archivos/index.html");
+/*
+ * Si el codigo lo pongo en este lado, 
+ * entonces el servidor solo lo 
+ * leera una vez
+ */
 
 http.createServer(function(req, res) {
-	res.write(html);
-	res.end();
+	/*
+	 * Si el codigo lo pongo en esta seccion,
+	 * entonces el servidor correra por cada peticion 
+	 * que se haga al navegador
+	 */
+	fs.readFile("archivos/index.html", function(err, data) {
+		res.write(data);
+		res.end();
+	});	
 }).listen(8080);
