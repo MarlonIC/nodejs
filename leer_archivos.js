d0e1790b37216d119c54e2cff4c5d387fcc6bb04
root <root@debian.debian-linux>
2016-12-29 15:48:32 -0500

    Curso NodeJS

--- leer_archivos.js
+++ leer_archivos.js
@@ -1,8 +1,9 @@
 var http = require("http"),
 	fs = require("fs");
 
-var html = fs.readFileAsync("/archivos/index.html");
+var html = fs.readFileSync("archivos/index.html");
 
 http.createServer(function(req, res) {
-
+	res.write(html);
+	res.end();
 }).listen(8080);
